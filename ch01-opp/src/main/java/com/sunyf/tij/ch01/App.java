8c8b1dc322743c5d8d1f39ce22167d6222ae57b4
sunyifeng <9339996@qq.com>
2018-10-19 19:08:03 +0800

    initial

---
+++ ch01-opp/src/main/java/com/sunyf/tij/ch01/App.java
@@ -0,0 +1,14 @@
+package com.sunyf.tij.ch01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ch06-authority/src/main/java/com/sunyf/tij/ch06/App.java
@@ -0,0 +1,13 @@
+package com.sunyf.tij.ch06;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
