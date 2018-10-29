cb9595a14a8b064220f57de8587fa9d8a674e751
sunyifeng <9339996@qq.com>
2018-10-29 13:54:02 +0800

    update

--- ch08-polymorphism/src/main/java/com/sunyf/tij/ch08/demo1/D.java
+++ ch08-polymorphism/src/main/java/com/sunyf/tij/ch08/demo1/D.java
@@ -2,7 +2,7 @@ package com.sunyf.tij.ch08.demo1;
 
 /**
  * @program: thinking-in-java
- * @description: CSCN 理解java的三大特性之多态
+ * @description: CSDN 理解java的三大特性之多态
  * @author: Mr.Sun
  * @create: 2018-10-24 11:03
  **/
--- ch08-polymorphism/src/main/java/com/sunyf/tij/ch08/demo1/Main.java
+++ ch08-polymorphism/src/main/java/com/sunyf/tij/ch08/demo1/Main.java
@@ -2,10 +2,10 @@ package com.sunyf.tij.ch08.demo1;
 
 /**
  * @program: thinking-in-java
- * @description: CSCN 理解java的三大特性之多态
+ * @description: CSDN 理解java的三大特性之多态
  *
- * 多态机制遵循的原则概括为：当超类对象引用变量引用子类对象时，
- * 被引用对象的类型而不是引用变量的类型决定了调用谁的成员方法，
+ * 多态机制遵循的原则概括为：
+ * 当超类对象的引用变量，引用子类对象时，被引用对象的类型，而不是引用变量的类型，决定了调用谁的成员方法，
  * 但是这个被调用的方法必须是在超类中定义过的，也就是说被子类覆盖的方法，
  * 但是它仍然要根据继承链中方法调用的优先级来确认方法，该优先级为：
  * this.show(O)、super.show(O)、this.show((super)O)、super.show((super)O)。
